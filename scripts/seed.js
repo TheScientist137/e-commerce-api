8b1e28db90020c5cac82cd426d095ca9223d912c
The-Scientist <thescientist137@outlook.es>
2024-12-08 13:49:43 +0100

    Sedded psql tables succesfully -- Created seedDataBase function on prisma/seed.js -- modified seed.json changing strings with integers

---
+++ scripts/seed.js
@@ -0,0 +1,27 @@
+const { PrismaClient } = require('@prisma/client');
+const seedData = require('../prisma/seed.json');
+
+const prisma = new PrismaClient();
+
+const seedDataBase = async () => {
+ try {
+  // Clean previous data
+  console.log('Cleaning previous data');
+  await prisma.telescopeType.deleteMany();
+  await prisma.telescope.deleteMany();
+
+  // Insert telescope type data
+  await prisma.telescopeType.createMany({
+   data: seedData.telescopeTypes
+  });
+  // Insert telescope data
+  await prisma.telescope.createMany({
+   data: seedData.telescopes
+  });
+
+ } catch (error) {
+    console.error('Error seeding database', error);
+ }
+}
+
+seedDataBase();
