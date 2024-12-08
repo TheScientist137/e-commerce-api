438295dc008a4f85fb48e70bc3158488c6d76795
The-Scientist <thescientist137@outlook.es>
2024-12-08 15:11:10 +0100

    Installed express framework

--- scripts/seed.js
+++ scripts/seed.js
@@ -6,18 +6,16 @@ const prisma = new PrismaClient();
 const seedDataBase = async () => {
  try {
   // Clean previous data
-  console.log('Cleaning previous data');
   await prisma.telescopeType.deleteMany();
   await prisma.telescope.deleteMany();
+  console.log('Cleaning previous data');
 
   // Insert telescope type data
-  await prisma.telescopeType.createMany({
-   data: seedData.telescopeTypes
-  });
+  await prisma.telescopeType.createMany({ data: seedData.telescopeTypes });
+  console.log('Seeded telescope types tables')
   // Insert telescope data
-  await prisma.telescope.createMany({
-   data: seedData.telescopes
-  });
+  await prisma.telescope.createMany({ data: seedData.telescopes });
+  console.log('Seeded telescope tables')
 
  } catch (error) {
     console.error('Error seeding database', error);
