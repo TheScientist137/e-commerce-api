1b261d3d8d71a9dcfa00cde0dca92e84f656cdee
The-Scientist <thescientist137@outlook.es>
2024-12-14 10:53:31 +0100

    Implemented express-session middleware -- started with simple track user with express-session (cookie of 1h) -- Next wi will implement passport.js for authentication and express-session for the cookie

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const session = require('express-session');
+import express from 'express';
+import session from 'express-session';
+
 const app = express();
 const PORT = 3000
 
+app.use(session({
+ secret: 'secret',
+ saveUninitialized: false,
+ resave: false,
+ cookie: { maxAge: 6000 * 60 } // 1h
+}));
+
 app.get('/', (req, res) => {
+ console.log(req.session);
+ console.log(req.session.id);
+ req.session.visited = true; // Base endpoint to save the session (stores cookie on sessionStore)
  res.send('Hello World!');
-})
+});
+
+app.get('/api/test', (req, res) => {
+ console.log(req.session.id);
+ res.send('test');
+});
 
 app.listen(PORT, () => {
  console.log(`App listening on port:, ${PORT}`);
