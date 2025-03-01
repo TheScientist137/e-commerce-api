005a8cac465aceeccb97bc529b86d1e7725807c6
The-Scientist <thescientist137@outlook.es>
2025-03-01 12:46:04 +0100

    archivo db.js para manejar la conexion a la base de datos

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import express from 'express';
-import session from 'express-session';
-import passport from 'passport';
-import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import shopRoutes from './routes/shopRoutes.js';
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-
-app.use(session({
- secret: 'secret',
- saveUninitialized: false,
- resave: false,
- cookie: { maxAge: 6000 * 60 }, name: 'auth' // 1h
-}));
-
-app.use(passport.initialize()); // Initialize passport middleware
-app.use(passport.session()); // Session manager middleware (Integrates express-session with passport)
-
-app.use(express.json()); // JSON request body middleware
-app.use(cors({
- origin: 'http://localhost:5173', // frontend url
- credentials: true // Permite que las cookies viajen entre solicitudes
-})); 
 
 app.use('/api/auth', authRoutes); // Authentication Routes
 app.use('/api/shop', shopRoutes); // Shop Routes
