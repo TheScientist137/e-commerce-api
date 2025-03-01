4a6d06ba0c93ae76d990d0921aa4158431beaac7
The-Scientist <thescientist137@outlook.es>
2025-03-01 12:53:56 +0100

    created schema.sql -- create database tables and relations -- execute schema.sql script on postgresql

---
+++ src/scripts/schema.sql
@@ -0,0 +1,24 @@
+-- users table
+CREATE TABLE users (
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(255) NOT NULL,
+ email VARCHAR(255) UNIQUE NOT NULL,
+ hashedPassword VARCHAR(255) NOT NULL
+);
+
+-- telescope_types table
+CREATE TABLE telescope_types (
+ id SERIAL PRIMARY KEY,
+ type VARCHAR(255) NOT NULL,
+ description TEXT
+);
+
+-- telescopes table
+CREATE TABLE telescopes (
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(255) NOT NULL,
+ description TEXT,
+ price DECIMAL(10, 2) NOT NULL,
+ brand VARCHAR(255) NOT NULL,
+ telescope_type_id INT REFERENCES telescope_types(id)
+);
