5197a552797256cee628f7d5061c5b7328dd18ad
The-Scientist <thescientist137@outlook.es>
2025-03-01 21:33:47 +0100

    created setup file to create schema.sql

---
+++ src/setup.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import pool from "./scripts/db.js";
+
+const schema = fs.readFileSync("./scripts/schema.sql", "utf-8");
+
+const setupDatabase = async () => {
+ try {
+  await pool.query(schema);
+  console.log("Esquema aplicado correctamente.");
+ } catch (error) {
+  console.error("Error al aplicar el esquema:", error);
+ } finally {
+  await pool.end(); // Cerrar la conexión
+ }
+};
+
+setupDatabase();
