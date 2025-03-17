6c61ac9fbc89fdea1935c9986cc42ac2f88649ac
The-Scientist <thescientist137@outlook.es>
2025-03-17 10:32:30 +0100

    added optical design types --last images on .json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors({
+app.use(cors({ // Cross Origin Resource Sharing
  origin: 'http://localhost:5173', // Frontend URL
  credentials: true // Allow credentials (cookies, Authorization headers)
 }));
