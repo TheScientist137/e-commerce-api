a678acf23b456e6bfb0cc4162c411ffdaa3d1804
The-Scientist <thescientist137@outlook.es>
2025-03-26 16:54:44 +0100

    get user route and controller

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -58,9 +58,10 @@ export const loginController = async (req, res) => {
       { expiresIn: '1h' } // expiration time
     );
 
-    res.status(200).json({ message: 'Login succesfully', token, email: user.email, role: user.role });
+    res.status(200).json({ message: 'Login succesfully', token, user: { email: user.email, role: user.role } });
   } catch (error) {
-    res.status(500).json({ message: 'Error during login', error: error.message });
+    console.error('Error during login', error);
+    res.status(500).json({ message: 'Server error - Error during login' });
   }
 }
 
@@ -71,3 +72,21 @@ export const logoutController = (req, res) => {
     res.status(500).json({ message: 'Error during logout', error: error.message });
   }
 }
+
+export const getCurrentUserController = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const user = await pool.query(
+      'SELECT id, name, email, role FROM users WHERE id = $1',
+      [userId]);
+
+    if (user.rows.length === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user.rows[0]);
+  } catch (error) {
+    console.error('Error obtaining current user', error);
+    res.status(500).json({ message: 'Server error - Error obtaining current user' });
+  }
+} 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { signupController, loginController, logoutController } from '../controllers/authControllers.js';
+import { authenticateJWT } from '../middleware/authMiddleware.js';
+import { 
+ signupController, 
+ loginController, 
+ logoutController,
+ getCurrentUserController } from '../controllers/authControllers.js';
 
 const router = express.Router();
 
 router.post('/signup', signupController);
 router.post('/login', loginController);
+router.get('/user', authenticateJWT, getCurrentUserController);
 router.post('/logout', logoutController);
 
 export default router;
